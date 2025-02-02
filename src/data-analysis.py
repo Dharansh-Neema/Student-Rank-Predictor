49923ecfd7aa251c53f8172b2a428161ff89e5c0
Dharansh Neema <dharanshneema@gmail.com>
2025-02-02 13:16:41 +0530

    Data visualization completed

--- src/data-analysis.py
+++ src/data-analysis.py
@@ -12,7 +12,9 @@ logger=setup_logger(name="data-analysis",log_file="logs/data-analysis.log",conso
 #Specifiying path 
 data_dir = "./data/processed/"
 analytics_dir = "./analytics/"
+visuals_dir = "./analytics/visuals" 
 os.makedirs(analytics_dir,exist_ok=True)
+os.makedirs(visuals_dir,exist_ok=True)
 
 
 def data_analysis(file_name:str)->None:
@@ -51,6 +53,47 @@ def data_analysis(file_name:str)->None:
         logger.error("Unexpected error occured while doing data analysis",e)
         raise
 
+def data_visualization(file_name:str)->None:
+    """This function visualise the data and 
+        plot it using matplotlib and save it on analytics folder"""
+    file_path = os.path.join(data_dir,file_name)
+    try:
+        df = pd.read_csv(file_path)
+        sns.set(style="whitegrid")
+        # 1. Scatter Plot: Score vs Accuracy
+        plt.figure(figsize=(8, 5))
+        sns.scatterplot(x=df["score"], y=df["accuracy"], hue=df["rank"], palette="coolwarm")
+        plt.title("Score vs Accuracy (Colored by Rank)")
+        plt.xlabel("Score")
+        plt.ylabel("Accuracy")
+        plt.savefig(os.path.join(visuals_dir, "score_vs_accuracy.png"))
+        plt.close()
+
+        # 2. Scatter Plot: Score vs Correct Answers ###
+        plt.figure(figsize=(8, 5))
+        sns.scatterplot(x=df["score"], y=df["correct_answers"], hue=df["rank"], palette="viridis")
+        plt.title("Score vs Correct Answers (Colored by Rank)")
+        plt.xlabel("Score")
+        plt.ylabel("Correct Answers")
+        plt.savefig(os.path.join(visuals_dir, "score_vs_correct_answers.png"))
+        plt.close()
+
+        # 3. Line Plot: Accuracy vs Rank ###
+        plt.figure(figsize=(8, 5))
+        sns.lineplot(x=df["rank"], y=df["accuracy"], marker="o", color="green")
+        plt.title("Accuracy vs Rank")
+        plt.xlabel("Rank")
+        plt.ylabel("Accuracy")
+        plt.savefig(os.path.join(visuals_dir, "accuracy_vs_rank.png"))
+        plt.close()
+        
+        logger.debug(f"All the visuals are save to {visuals_dir} succesfully.")
+
+    except Exception as e:
+        logger.error("Unexpected error occured",e)
+        raise
+
 if __name__ == "__main__":
     file_name = "previous_year_data.csv"
     data_analysis(file_name)
+    data_visualization(file_name)
